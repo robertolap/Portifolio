fbc1f86da0021af3239df3eb65fcf991748afee9
robertolap <lapetinaroberto@gmail.com>
2023-10-12 19:31:16 -0300

    test

--- src/cover.jsx
+++ src/cover.jsx
@@ -26,21 +26,6 @@ const Cover = () => {
         about the world of web development, focused on problem solving and the
         user experience.
       </p>
-
-      <div class="container">
-        <div class="content">
-          <div class="content__container">
-            <p class="content__container__text">Hello</p>
-
-            <ul class="content__container__list">
-              <li class="content__container__list__item">world !</li>
-              <li class="content__container__list__item">coder !</li>
-              <li class="content__container__list__item">users !</li>
-              <li class="content__container__list__item">uiverse</li>
-            </ul>
-          </div>
-        </div>
-      </div>
     </section>
   );
 };
