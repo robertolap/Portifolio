154eef0958ab58ebea9f853735bace3fab03a3d1
robertolap <lapetinaroberto@gmail.com>
2024-01-22 15:33:39 -0300

    cv

--- src/cover.jsx
+++ src/cover.jsx
@@ -29,7 +29,7 @@ const Cover = () => {
       </p>
       <a
         class="button"
-        href="https://drive.google.com/file/d/1iWlqG5SrGl_tnlYr7sQWFP3XrHtgvnnt/view?usp=sharing"
+        href="https://drive.google.com/file/d/16kYfs2m4m7kcAtQyIcQJwt9sRjWcdSrN/view?usp=sharing"
         target="_blank"
         rel="noopener noreferrer"
       >
