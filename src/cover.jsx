f9b7e10e6710a0465a5b1ff376c6c4dbedc1622d
Roberto Lapetina <52218154+robertolap@users.noreply.github.com>
2025-01-09 14:58:56 +0800

    Update cover.jsx

--- src/cover.jsx
+++ src/cover.jsx
@@ -21,7 +21,7 @@ const Cover = () => {
       <h2 className="hello">Hey there, I'm a </h2>
 
       <h1 className="front">Data</h1>
-      <h1 className="developer">Enginner</h1>
+      <h1 className="developer">Analyst</h1>
       <p className="intro">
         As a recent graduate in Computer Engineering and passionate about the
         field of data, I'm eager to delve into both data engineering and data
@@ -31,7 +31,7 @@ const Cover = () => {
       </p>
       <a
         class="button"
-        href="https://drive.google.com/file/d/1ur8K2iRm3R1l27VoeluVpJ0SG9AzgN3D/view?usp=sharing"
+        href="https://drive.google.com/file/d/1kTAtGvFHq4eSUQ-dN3-1rPk1QixBUUsW/view?usp=sharing"
         target="_blank"
         rel="noopener noreferrer"
       >
